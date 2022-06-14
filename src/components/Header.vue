d3c75ba66ccba87e2f24b39485a5efd5c9cd4eca
wang_chao <chao.wang@guanaitong.com>
2022-06-14 23:16:22 +0800

    移除header的下载

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="container header">
-    <a href="index.html" class="hidden-xs-only"><img class="logo" src="../assets/logo.png" alt=""></a>
+    <a href="index.html"><img class="logo" src="../assets/logo.png" alt=""></a>
     <div class="menu">
       <a href="index.html" :class="'index' === current ? 'current' : ''">快剪</a>
       <a href="fastdownloader.html" :class="'fastdownloader' === current ? 'current' : ''">快下载</a>
       <a href="docs/index.html" target="_blank">教程</a>
-      <a href="#download">下载</a>
       <a :href="'buy.html?appCode=' + appCode" target="_blank" :class="'buy' === current ? 'current' : ''">价格</a>
     </div>
   </div>
